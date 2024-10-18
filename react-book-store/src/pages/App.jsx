4ea413ae1a2ec1346cb03204a9c73d9c9bd9c993
emondewan00 <dewanmohammademon@gmail.com>
2024-10-19 02:00:25 +0600

    refactor home page and wishlist page also added skeleton on both pages

--- react-book-store/src/pages/App.jsx
+++ react-book-store/src/pages/App.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const App = () => {
   const [url, setUrl] = useState("https://gutendex.com/books/?page=1");
   const [searchText, setSearchText] = useState("");
   const [wishlistIds, setWishlistIds] = useState([]);
+  const [fetchStatus, setFetchStatus] = useState("idle");
 
   const [books, setBooks] = useState({
     count: 0,
@@ -27,13 +27,21 @@ const App = () => {
 
   useEffect(() => {
     const getBooks = async () => {
+      setFetchStatus("loading");
       let u = url;
       if (searchText) {
         u = `${url}&search=${searchText}`;
       }
-      const response = await fetch(u);
-      const data = await response.json();
-      setBooks(data);
+
+      try {
+        const response = await fetch(u);
+        const data = await response.json();
+        setBooks(data);
+        setFetchStatus("success");
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setFetchStatus("error");
+      }
     };
     getBooks();
   }, [url, searchText]);
@@ -71,7 +79,6 @@ const App = () => {
           <p className="text-lg text-white mb-4">
             Find books by name or publication year
           </p>
-
           <div className="grid grid-cols-3 gap-4 shadow-md rounded-md bg-white p-8 min-w-[30vw] w-full">
             <div className="col-span-2">
               <label
@@ -86,10 +93,10 @@ const App = () => {
                 id="search"
                 placeholder="Type your search words"
                 className="border border-gray-200 rounded w-full p-2 focus:outline-none"
+                value={searchText}
                 onChange={(e) => onQueryChange(e.target.value)}
               />
             </div>
-
             <div>
               <label
                 htmlFor="filterBy"
@@ -112,77 +119,110 @@ const App = () => {
           </div>
         </div>
       </div>
+
       <div className="my-40 max-w-6xl mx-auto">
-        <div className="grid grid-cols-5 gap-4">
-          {books.results.map((book) => (
-            <div key={books.id} className="space-y-3 group">
-              <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4 h-[200px]">
-                <img
-                  className="h-full object-contain max-w-[144px] hover:scale-105 duration-150 delay-75 ease-in-out"
-                  src={book.formats["image/jpeg"]}
-                  alt={book.title}
-                />
-              </div>
-
-              <div className="space-y-3">
-                <Link
-                  to={`/book/${book.id}`}
-                  className="text-lg font-bold lg:text-xl line-clamp-1 group-hover:text-blue-500"
-                >
-                  {book.title}
-                </Link>
-                <p className="text-xs lg:text-sm line-clamp-1">
-                  By : <span>{book?.authors[0]?.name}</span>
-                </p>
-                <div className="flex items-center justify-between">
-                  <h4 className="text-lg font-semibold">
-                    Type: {book.media_type}
-                  </h4>
-
-                  <p className="text-xs lg:text-sm"> Id:{book.id}</p>
+        {fetchStatus === "loading" && (
+          <div className="grid grid-cols-5 gap-4">
+            {skeleton.map((i) => (
+              <div key={i} className="space-y-3 group animate-pulse">
+                <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4 h-[200px]">
+                  <div className="h-full w-[144px] bg-gray-300 rounded-md" />
                 </div>
-
-                <button
-                  onClick={() => toggleWishlist(book.id)}
-                  className={`flex items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%] lg:py-2 w-full ${
-                    wishlistIds.includes(book.id)
-                      ? "bg-[#DC2954]/[14%] text-[#DC2954] transition-all hover:bg-[#DC2954]/[24%]"
-                      : "bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%]"
-                  }`}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill={wishlistIds.includes(book.id) ? "#DC2954" : "none"}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-5 w-5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
-                  </svg>
-                  Add to Wishlist
-                </button>
+                <div className="space-y-3">
+                  <div className="h-6 bg-gray-300 rounded-md lg:h-8" />
+                  <div className="h-4 bg-gray-300 rounded-md lg:h-5" />
+                  <div className="flex items-center justify-between">
+                    <div className="h-6 bg-gray-300 rounded-md lg:h-8 w-1/2" />
+                    <div className="h-4 bg-gray-300 rounded-md lg:h-5 w-1/4" />
+                  </div>
+                  <button className="flex items-center justify-center gap-1 rounded-md py-1.5 bg-gray-300 animate-pulse w-full">
+                    <div className="h-5 w-5 bg-gray-300 rounded-full" />
+                    <span className="h-4 bg-gray-300 rounded-md w-1/2" />
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
+        {fetchStatus === "error" && (
+          <p className="text-center text-red-500">Error fetching books.</p>
+        )}
+        {fetchStatus === "success" && (
+          <div className="grid grid-cols-5 gap-4">
+            {books?.results?.map((book) => (
+              <div key={book.id} className="space-y-3 group">
+                <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4 h-[200px]">
+                  <img
+                    className="h-full object-contain max-w-[144px] hover:scale-105 duration-150 delay-75 ease-in-out"
+                    src={book.formats["image/jpeg"]}
+                    alt={book.title}
+                  />
+                </div>
+                <div className="space-y-3">
+                  <Link
+                    to={`/book/${book.id}`}
+                    className="text-lg font-bold lg:text-xl line-clamp-1 group-hover:text-blue-500"
+                  >
+                    {book.title}
+                  </Link>
+                  <p className="text-xs lg:text-sm line-clamp-1">
+                    By : <span>{book?.authors[0]?.name}</span>
+                  </p>
+                  <div className="flex items-center justify-between">
+                    <h4 className="text-lg font-semibold">
+                      Type: {book.media_type}
+                    </h4>
+                    <p className="text-xs lg:text-sm">Id: {book.id}</p>
+                  </div>
+                  <button
+                    onClick={() => toggleWishlist(book.id)}
+                    className={`flex items-center justify-center gap-1 rounded-md py-1.5 transition-all w-full ${
+                      wishlistIds.includes(book.id)
+                        ? "bg-[#DC2954]/[14%] text-[#DC2954] hover:bg-[#DC2954]/[24%]"
+                        : "bg-[#1C4336]/[14%] text-[#1C4336] hover:bg-[#1C4336]/[24%]"
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill={wishlistIds.includes(book.id) ? "#DC2954" : "none"}
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-5 w-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                      />
+                    </svg>
+                    Add to Wishlist
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
         <div className="flex justify-end gap-x-4 mt-2">
           <button
-            className="flex items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%] px-5"
+            className={`flex items-center justify-center gap-1 rounded-md py-1.5 transition-all px-5 ${
+              books.previous
+                ? "bg-[#1C4336]/[14%] text-[#1C4336] hover:bg-[#1C4336]/[24%]"
+                : "bg-gray-300 text-gray-600 cursor-not-allowed"
+            }`}
             onClick={() => setUrl(books.previous)}
-            disabled={books.previous === null}
+            disabled={!books.previous}
           >
             Previous
           </button>
-
           <button
-            className="flex items-center justify-center gap-1 rounded-md bg-[#1C4336] py-1.5 text-white transition-all hover:opacity-80 px-5"
+            className={`flex items-center justify-center gap-1 rounded-md py-1.5 transition-all px-5 ${
+              books.next
+                ? "bg-[#1C4336] text-white hover:opacity-80"
+                : "bg-gray-300 text-gray-600 cursor-not-allowed"
+            }`}
             onClick={() => setUrl(books.next)}
-            disabled={books.next === null}
+            disabled={!books.next}
           >
             Next
           </button>
--- react-book-store/src/pages/Wishlist.jsx
+++ react-book-store/src/pages/Wishlist.jsx
@@ -19,51 +19,59 @@ const languages = {
   ur: "Urdu",
 };
 
+const skeleton = [0, 1, 2, 3];
+
 const Wishlist = () => {
   const [url, setUrl] = useState("https://gutendex.com/books/");
-  const [wishlistIds, setWishlistIds] = useState([]);
+  const [status, setStatus] = useState("loading");
   const [books, setBooks] = useState({
     count: 0,
     next: null,
     previous: null,
     results: [],
   });
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const wishlistItems = JSON.parse(localStorage.getItem("wishlistItems"));
-    if (wishlistItems) {
-      setWishlistIds(wishlistItems);
-    }
-  }, []);
+    const wishlistItems =
+      JSON.parse(localStorage.getItem("wishlistItems")) || [];
+    setStatus("loading");
 
-  useEffect(() => {
-    if (wishlistIds.length > 0) {
-      let query = url + `?ids=${wishlistIds.join(",")}`;
+    if (wishlistItems.length > 0) {
+      const query = `${url}?ids=${wishlistItems.join(",")}`;
+
       const getBooks = async () => {
-        const response = await fetch(query);
-        const data = await response.json();
-        setBooks(data);
-      };
-      getBooks();
-      console.log(query, "query");
-    }
-  }, [url, wishlistIds]);
+        try {
+          const response = await fetch(query);
 
-  const toggleWishlist = (id) => {
-    setWishlistIds((prevIds) => {
-      const currentIds = [...prevIds]; // Create a copy of the current state
-      const index = currentIds.indexOf(id);
-      if (index === -1) {
-        currentIds.push(id); // Add to wishlist if not found
-      } else {
-        currentIds.splice(index, 1); // Remove if already exists
-      }
-      localStorage.setItem("wishlistItems", JSON.stringify(currentIds)); // Update localStorage
-      return currentIds; // Update state
-    });
+          if (!response.ok) {
+            throw new Error("Failed to fetch books");
+          }
+
+          const data = await response.json();
+          setBooks(data);
+          setStatus("loaded");
+        } catch (err) {
+          setError(err.message);
+          setStatus("error");
+        }
+      };
+
+      getBooks();
+    } else {
+      setStatus("loaded");
+    }
+  }, [url]);
+
+  const deleteBook = (id) => {
+    const filteredBooks = books.results.filter((book) => book.id !== id);
+    setBooks((prevBooks) => ({ ...prevBooks, results: filteredBooks }));
+    const wishlistItems =
+      JSON.parse(localStorage.getItem("wishlistItems")) || [];
+    const updatedWishlist = wishlistItems.filter((itemId) => itemId !== id);
+    localStorage.setItem("wishlistItems", JSON.stringify(updatedWishlist));
   };
 
-  console.log(books);
   return (
     <div>
       <div className="bg-gray-100 h-40 flex items-center justify-center">
@@ -71,64 +79,171 @@ const Wishlist = () => {
           Wishlist
         </h1>
       </div>
-      <table className="w-full max-w-5xl mx-auto mt-10">
-        <thead className="w-full">
-          <tr className="hidden md:table-row border-b *:pb-2">
-            <th className="text-start w-1/3">Product</th>
-            <th className="text-start w-1/6">Price</th>
-            <th className="text-start w-1/6">Stock Status</th>
-            <th className="text-start w-1/6">Action</th>
-          </tr>
-        </thead>
-        <tbody className="cart md:*:!border-b">
-          {books.results.map((book) => (
-            <tr
-              key={book.id}
-              className="flex flex-col items-center justify-center border  p-4 m-4 md:!border-x-0 md:p-0 md:m-0 md:table-row md:*:my-2"
-            >
-              <td className="md:grid grid-cols-3 items-center gap-x-4 group">
-                <img
-                  src={book.formats["image/jpeg"]}
-                  alt={book.title}
-                  className="object-contain h-40 p-4 w-40 mx-auto md:mx-auto"
-                />
-                <div className="col-span-2 md:text-start text-center">
-                  <Link
-                    to={`/book/${book.id}`}
-                    className="text-xl md:text-lg my-2 md:my-0 group-hover:text-blue-500"
-                  >
-                    {book.title}
-                  </Link>
-                  <p>Downloaded: {book.download_count}</p>
-                </div>
-              </td>
-              <td className="text-center md:text-start">
-                By: <span>{book?.authors?.[0]?.name || "Unknown Author"}</span>
-                <p>
-                  Born:{" "}
-                  {book?.authors?.[0]?.birth_year
-                    ? book.authors[0].birth_year
-                    : "N/A"}{" "}
-                  -{" "}
-                  {book?.authors?.[0]?.death_year
-                    ? book.authors[0].death_year
-                    : "Present"}
-                </p>
-              </td>
-              <td>{languages[book.languages?.[0]] || "Unknown"}</td>
-              <td>
-                <button
-                  onClick={() => toggleWishlist(book.id)}
-                  className={`flex items-center justify-center gap-1 rounded-md  py-1.5  lg:py-2 w-full bg-[#DC2954]/[14%] text-[#DC2954] transition-all hover:bg-[#DC2954]/[24%]
-                  `}
+
+      {status === "loading" && (
+        <div className="w-full max-w-5xl mx-auto mt-10">
+          <table>
+            <thead className="w-full">
+              <tr className="hidden md:table-row border-b pb-2">
+                <th className="text-start">Book</th>
+                <th className="text-start w-1/6">Author</th>
+                <th className="text-start w-1/6">Languages</th>
+                <th className="text-start w-1/6">Action</th>
+              </tr>
+            </thead>
+            <tbody className=" md:border-b">
+              {skeleton.map((i) => (
+                <tr
+                  key={i}
+                  className="flex flex-col items-center justify-center border p-4 m-4 md:!border-x-0 md:p-0 md:m-0 md:table-row animate-pulse"
                 >
-                  Remove from Wishlist
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                  <td className="md:grid grid-cols-3 items-center gap-x-4">
+                    <div className="h-40 w-40 bg-gray-300 mx-auto rounded-md" />
+                    <div className="col-span-2 md:text-start text-center">
+                      <div className="h-6 bg-gray-300 rounded-md w-3/4 mx-auto mb-2" />
+                      <div className="h-4 bg-gray-300 rounded-md w-1/2 mx-auto" />
+                    </div>
+                  </td>
+                  <td className="text-center md:text-start">
+                    <div className="h-4 bg-gray-300 rounded-md w-1/2 mx-auto mb-1" />
+                    <div className="h-4 bg-gray-300 rounded-md w-3/4 mx-auto" />
+                  </td>
+                  <td>
+                    <div className="h-4 bg-gray-300 rounded-md w-3/4" />
+                  </td>
+                  <td>
+                    <button className="flex items-center justify-center gap-1 rounded-md py-1.5 lg:py-2 px-4 bg-gray-300 animate-pulse w-full">
+                      <div className="h-5 w-5 bg-gray-300 rounded-full" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {status === "error" && (
+        <div className="flex justify-center items-center h-[75vh]">
+          <p className="text-xl text-red-500">{error}</p>
+        </div>
+      )}
+
+      {status === "loaded" && (
+        <div className="w-full max-w-5xl mx-auto mt-10">
+          {books.results.length === 0 ? (
+            <div className="flex justify-center items-center h-[70vh]">
+              <p className="text-xl">Your wishlist is empty.</p>
+            </div>
+          ) : (
+            <>
+              <table>
+                <thead className="w-full">
+                  <tr className="hidden md:table-row border-b pb-2">
+                    <th className="text-start">Book</th>
+                    <th className="text-start w-1/6">Author</th>
+                    <th className="text-start w-1/6">Languages</th>
+                    <th className="text-start w-1/6">Action</th>
+                  </tr>
+                </thead>
+                <tbody className=" md:border-b">
+                  {books.results.map((book) => (
+                    <tr
+                      key={book.id}
+                      className="flex flex-col items-center justify-center border p-4 m-4 md:!border-x-0 md:p-0 md:m-0 md:table-row"
+                    >
+                      <td className="md:grid grid-cols-3 items-center gap-x-4 group">
+                        <img
+                          src={book.formats["image/jpeg"]}
+                          alt={book.title}
+                          className="object-contain h-40 p-4 w-40 mx-auto"
+                        />
+                        <div className="col-span-2 md:text-start text-center">
+                          <Link
+                            to={`/book/${book.id}`}
+                            className="text-xl md:text-lg my-2 md:my-0 group-hover:text-blue-500"
+                          >
+                            {book.title}
+                          </Link>
+                          <p>Downloaded: {book.download_count}</p>
+                        </div>
+                      </td>
+                      <td className="text-center md:text-start">
+                        By:{" "}
+                        <span>
+                          {book?.authors?.[0]?.name || "Unknown Author"}
+                        </span>
+                        <p>
+                          Born: {book?.authors?.[0]?.birth_year || "N/A"} -{" "}
+                          {book?.authors?.[0]?.death_year || "Present"}
+                        </p>
+                      </td>
+                      <td>
+                        {book?.languages?.map((language, i) => (
+                          <span key={i}>
+                            {languages[language] || "Unknown"}
+                          </span>
+                        ))}
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => deleteBook(book.id)}
+                          className="flex items-center justify-center gap-1 rounded-md py-1.5 lg:py-2 px-4 bg-[#DC2954]/[14%] text-[#DC2954] transition-all hover:bg-[#DC2954]/[24%]"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <polyline points="3 6 5 6 21 6"></polyline>
+                            <path d="M19 6l-1.5 14.5a2 2 0 0 1-2 1.5H8.5a2 2 0 0 1-2-1.5L5 6"></path>
+                            <path d="M10 11v6"></path>
+                            <path d="M14 11v6"></path>
+                            <path d="M5 6l1-4h12l1 4"></path>
+                          </svg>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+
+              {books.results.length > 0 && (
+                <div className="flex justify-end gap-x-4 mt-2">
+                  <button
+                    className={`flex items-center justify-center gap-1 rounded-md py-1.5 transition-all px-5 ${
+                      books.previous
+                        ? "bg-[#1C4336]/[14%] text-[#1C4336] hover:bg-[#1C4336]/[24%]"
+                        : "bg-gray-300 text-gray-600 cursor-not-allowed"
+                    }`}
+                    onClick={() => setUrl(books.previous)}
+                    disabled={!books.previous}
+                  >
+                    Previous
+                  </button>
+                  <button
+                    className={`flex items-center justify-center gap-1 rounded-md py-1.5 transition-all px-5 ${
+                      books.next
+                        ? "bg-[#1C4336] text-white hover:opacity-80"
+                        : "bg-gray-300 text-gray-600 cursor-not-allowed"
+                    }`}
+                    onClick={() => setUrl(books.next)}
+                    disabled={!books.next}
+                  >
+                    Next
+                  </button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 };
